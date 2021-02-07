f5ade24362a4eac6fecd540f4afba7d1dd260cb6
TsilavinaTB <77960847+TsilavinaTB@users.noreply.github.com>
2021-02-07 17:44:39 +0300

    Update Connexion.php

--- src/Connexion.php
+++ src/Connexion.php
@@ -4,8 +4,8 @@ namespace App;
 class Connexion  {
 
     public static function getPDO (){
-        return new \PDO("sqlite:data/data.sql", null, null, [
-            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        return new PDO('mysql:host=sql7.freesqldatabase.com:3306;sql7390456', 'sql7390456', 'S5yVTQSP1d', [
+            PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
         ]);
     }
 }
