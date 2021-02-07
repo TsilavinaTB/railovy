f99ce8fbfd615c648b088b177177df71eb197163
TsilavinaTB <77960847+TsilavinaTB@users.noreply.github.com>
2021-02-07 19:08:31 +0300

    Update Connexion.php

--- src/Connexion.php
+++ src/Connexion.php
@@ -4,8 +4,8 @@ namespace App;
 class Connexion  {
 
     public static function getPDO (){
-        return new PDO('mysql:host=sql7.freesqldatabase.com:3306;sql7390456', 'sql7390456', 'S5yVTQSP1d', [
-            PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        return new \PDO('mysql:host=sql7.freesqldatabase.com:3306;sql7390456', 'sql7390456', 'S5yVTQSP1d', [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
         ]);
     }
 }
