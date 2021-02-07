36b409da70767121540f14c933f488ea4cfcec6a
TsilavinaTB <77960847+TsilavinaTB@users.noreply.github.com>
2021-02-07 19:05:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-echo 'tst';
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+require 'vendor/autoload.php';
+
+use App\Connexion;
+
+$pdo = Connexion::getPDO();
+
 
 ?>
+
+
+
+<a href="Bots/youtubeUrl.php">Youtube APIs</a>
