5f376bdefe1b1cbea51fe8b613b1685ebbe9b712
TsilavinaTB <77960847+TsilavinaTB@users.noreply.github.com>
2021-02-12 17:51:53 +0300

    Update Test.php

--- Bots/test.php
+++ Bots/test.php
@@ -1,43 +1,41 @@
 {
-"messages": [
-{
-"attachment":{
-"type":"template",
-"payload":{
-"template_type":"generic",
-"image_aspect_ratio": "square",
-"elements":[
-{
-"title":"Chatfuel Rockets Jersey",
-"image_url":"https://rockets.chatfuel.com/assets/shirt.jpg",
-"subtitle":"Size: M",
-"buttons":[
-{
-"type":"web_url",
-"url":"https://rockets.chatfuel.com/store",
-"title":"View Item"
-}
-]
-},
-{
-"title":"Chatfuel Rockets Jersey",
-"image_url":"https://rockets.chatfuel.com/assets/shirt.jpg",
-"subtitle":"Size: L",
-"default_action": {
-"type": "web_url",
-"url": "https://rockets.chatfuel.com/store"
-},
-"buttons":[
-{
-"type":"web_url",
-"url":"https://rockets.chatfuel.com/store",
-"title":"View Item"
-}
-]
-}
-]
-}
-}
-}
-]
+    "messages": [
+        {
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"generic",
+                    "image_aspect_ratio": "square",
+                    "elements":[
+                        {
+                            "title":"test 1",
+                            "image_url":"https://rockets.chatfuel.com/assets/shirt.jpg",
+                            "subtitle":"Size: M",
+                            "buttons":[
+                                {
+                                    "type": "show_block",
+                                    "block_names": ["testSow"],
+                                    "title": "Telecharger" ,
+                                    "set_attributes": {"click": "test 1"}                          
+                                }
+                            ]
+                        },
+                        {
+                            "title":"test 2",
+                            "image_url":"https://rockets.chatfuel.com/assets/shirt.jpg",
+                            "subtitle":"Size: M",
+                            "buttons":[
+                                {
+                                    "type": "show_block",
+                                    "block_names": ["testSow"],
+                                    "title": "Telecharger" ,
+                                    "set_attributes": {"click": "test 2"}                          
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    ]
 }
