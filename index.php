a9cd385fff9649eed0cc2528667e34898497cc26
TsilavinaTB <77960847+TsilavinaTB@users.noreply.github.com>
2021-02-17 11:54:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <h1>TEST RAILOVY</h1>
+<?php
+$rslt =  file_put_contents("Tmpfile.zip", file_get_contents("http://someurl/file.zip"));
+var_dump($rslt);
+?>
