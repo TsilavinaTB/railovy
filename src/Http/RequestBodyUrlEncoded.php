31625ac892c5cc750fd8317fbfa61e2eec57ac5f
TsilavinaTB <77960847+TsilavinaTB@users.noreply.github.com>
2021-02-18 09:55:39 +0300

    Update RequestBodyUrlEncoded.php

--- src/Http/RequestBodyUrlEncoded.php
+++ src/Http/RequestBodyUrlEncoded.php
@@ -20,7 +20,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-namespace Facebook\Http;
+namespace App\Http;
 
 /**
  * @package Facebook
