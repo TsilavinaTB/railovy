a46b7d52a5fdf47b4e831be16810f3fa743e41c4
TsilavinaTB <77960847+TsilavinaTB@users.noreply.github.com>
2021-02-20 15:43:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,14 +13,14 @@ use Facebook\Facebook;
   'app_id' => 2891856974472829,
   'app_secret' => '98be3ab63d0d2b76a3ef55facf760f3c',
   'default_graph_version' => 'v2.10',
-  'default_access_token' => 'EAApGIOu0ZAn0BAIJ5PGbkvKbeCgzZCzpdZCSmsKX5AvlGMRyNC7m09aBsammZA6c2rY2NcwZA42GvEYCBPYrFntt837XJ20ZAxI1PbZBWkoFb7mgAoATyTVnG6Lz6NTEuRRMhrt135dNa6KcY0etFLLqk4l25XGWs4XZC0gxjuQFdOCqdsMCZBZBRCvvMLmAkeKnPGkbqEvLF8KeSsEJ5uYsDczo1WvEv4SbiME8clJl7CfwZDZD', // optional
+  //'default_access_token' => '', // optional
 ]);
 
 # login.php
 
 $helper = $fb->getRedirectLoginHelper();
 $permissions = ['email', 'user_likes']; // optional
-$loginUrl = $helper->getLoginUrl('http://tsilavina.herokuapp.com', $permissions);
+$loginUrl = $helper->getLoginUrl('http://tsilavina.herokuapp.com/index.php', $permissions);
 
 echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 
