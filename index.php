05786e09186258011115a68dbaa5d0760288fa14
TsilavinaTB <77960847+TsilavinaTB@users.noreply.github.com>
2021-02-23 10:42:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $loginUrl = $helper->getLoginUrl('http://tsilavina.herokuapp.com/index.php', $pe
 
 echo '<a href="' . $loginUrl . '">Log in with Facebook!</a>';
 
-//$response = $fb->get('');
-//$response = $response->getGraphUser();
-var_dump($helper);
+$response = $fb->get('');
+$response = $response->getGraphUser();
+var_dump($response);
 ?>
